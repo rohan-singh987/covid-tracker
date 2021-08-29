22e40579aed56a7b157c96665aeb66f9f0bbb1bb
Rohan Singh <rohannsingh98765@gmail.com>
2021-08-29 23:27:28 +0530

    Uploading Files

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+We will start our project with Structuring components
+        
+        {/* Header */}
+      {/* Title + Dropdown box */}
+
+      {/* InfoBox --> cases */}
+      {/* InfoBox --> recovery */}
+      {/* InfoBox --> deaths */}
+
+      {/* Table */}
+      {/* graph */}
+      
+      {/* Map */}
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* Header */}
+      {/* Title + Dropdown box */}
+
+      {/* InfoBox --> cases */}
+      {/* InfoBox --> recovery */}
+      {/* InfoBox --> deaths */}
+
+      {/* Table */}
+      {/* graph */}
+      
+      {/* Map */}
+
+      
+    </div>
+  );
+}
+
+export default App;
