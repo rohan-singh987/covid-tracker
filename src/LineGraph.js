fc5be5126525c2753bbcd8a96a1694ae141e4893
Rohan Singh <rohannsingh98765@gmail.com>
2021-09-05 00:29:01 +0530

    some small changes

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Line } from "react-chartjs-2"
 
 
@@ -11,8 +11,8 @@ function LineGraph() {
         const chartData = [];
         let lastDataPoint;
 
-       for(let date in data.cases) {
-            if (lastDataPoint){
+        for (let date in data.cases) {
+            if (lastDataPoint) {
                 const newDataPoint = {
                     x: date,
                     y: data[caseType][date] - lastDataPoint
@@ -27,22 +27,22 @@ function LineGraph() {
 
     useEffect(() => {
         fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then(response => response.json())
-        .then(data =>{
+            .then(response => response.json())
+            .then(data => {
 
-            console.log(data);
-            const chartData = buildChartData(data)
-            setData(chartData)
-        })
+                console.log(data);
+                const chartData = buildChartData(data)
+                setData(chartData)
+            })
 
     }, []);
 
-    
+
 
     return (
         <div>
             <Line data={{
-               datasets : [{data: data,},] 
+                datasets: [{ data: data, },]
             }} />
         </div>
     )
